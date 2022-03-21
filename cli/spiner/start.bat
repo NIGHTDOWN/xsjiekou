a39f8ce12479720447d5efdcff0c7b4887dfa589
Yzw <yzw@example.com>
2022-03-22 00:28:46 +0800

    submit

--- cli/spiner/start.bat
+++ cli/spiner/start.bat
@@ -1,4 +1,20 @@
 :start
 php mtxt_cn.php
 php mtoon_cn.php
+php cart_aikan.php
+php hinovel_els.php
+php hinovel_en.php
+php hinovel_id.php
+php hinovel_th.php
+php hinovel_vi.php
+php lanovel_en.php
+php lanovel_ms.php
+php lanovel_th.php
+php hinovel_vi.php
+php mtoon_cn.php
+php mtoon_en.php
+php mtoon_id.php
+php mtoon_th.php
+php mtoon_vi.php
+php mtxt_id.php
 goto start
