c02a860185e83711fa5b172a50f1fd1423d69350
yzw <yzw@ng169.com>
2023-04-06 22:53:39 +0800

    dsfsdf

--- source/core/lib/Tcp.php
+++ source/core/lib/Tcp.php
@@ -28,7 +28,7 @@ class Tcp extends Socket
 		} else {
 			self::$master = stream_socket_server("tcp://{$host}:{$port}", $errno, $errmsg, STREAM_SERVER_BIND | STREAM_SERVER_LISTEN);
 		}
-d("gdfgdfgdf");
+
 		if (!self::$master) {
 			self::error("错误代码：$errno--$errmsg \n");
 		}
@@ -61,8 +61,10 @@ d("gdfgdfgdf");
 
 
 		if (self::$EPOLL) {
+			d(11111);
 			$this->epollmodel();
 		} else {
+			d(222222);
 			while (!self::$stop) {
 				$this->selectmodel();
 			}
