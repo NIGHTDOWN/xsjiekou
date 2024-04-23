7100dda21fc1891ffc59ad6391f3a45d5cf0bf74
Your Name <you@example.com>
2024-04-23 16:46:45 +0800

    tb spiner

---
+++ cli/spiner/taobao.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * 本服务接收两个参数  IP 端口
+ * 列子 ：php opsock 192.168.1.1 8080
+ */
+
+
+
+
+
+require_once (dirname(__FILE__)) . "/spbase/txt_qq.php";
+
+
+
+
+
+
+//俄罗斯
+$ob = new taobaobase();
+// $ob->_booklang = 7;
+// $ob->_bookdstdesc = '俄罗斯_hinovel';
+// $ob->_domian = "https://api.hinoveleurope.com";
+// $ob->appneedinfo['lang'] = 'ru';
+// $ob->appneedinfo2['column_id'] = '5';
+
+// $ob->initsp();
+// $ob->reg();
+$ob->start();
