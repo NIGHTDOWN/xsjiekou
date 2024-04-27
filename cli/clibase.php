ef6c222390e1f34ecd6a5b2c86a50bbbb0d3057a
Your Name <you@example.com>
2024-04-28 03:50:37 +0800

    sdfsd

--- cli/clibase.php
+++ cli/clibase.php
@@ -546,7 +546,7 @@ class Clibase  extends Cli
             $this->setproxy($this->ip, $this->port);
         }
         $url = $this->domian . $api;
-        d($this);
+        d($this,1);
         if (php_sapi_name() === 'cli') {
         d($url);}
         $data = $this->spiner->get($url, $time);
