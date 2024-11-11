3f61c4b2cdec771e07687fe26e37a897c6c64a29
杨志伟1 <752942639@qq.com>
2024-11-11 22:13:34 +0800

    sdfsfsdf

--- source/core/tool/ngSwoole.php
+++ source/core/tool/ngSwoole.php
@@ -16,14 +16,26 @@ class ngSwoole
     $this->http->on('request', function ($request, $response) {
       $response->end("Welcome to WebSocket chat room!");
     });
+    // 设置跨域头部，允许所有来源
+    $this->http->on('request', function ($request,  $response) {
+      $response->header('Access-Control-Allow-Origin', '*');
+      $response->header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+      $response->header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+
+      if ($request->server['request_uri'] == '/ws') {
+        // 如果是 WebSocket 握手请求，不做任何响应，因为 Swoole 会自动处理
+        return;
+      }
+
+      $response->end("Welcome to WebSocket chat room!");
+    });
 
 
-
- // Bug 修复：添加 onMessage 回调处理函数
-  $this->http->on('message', function ($ws, $frame) {
-    echo "Received message: {$frame->data}\n";
-    $ws->push($frame->fd, "Server: ". $frame->data);
-  });
+    // Bug 修复：添加 onMessage 回调处理函数
+    $this->http->on('message', function ($ws, $frame) {
+      echo "Received message: {$frame->data}\n";
+      $ws->push($frame->fd, "Server: " . $frame->data);
+    });
     $ws = $this->http->on('websocket', function ($ws, $frame) {
       static $clients = [];
       if ($frame->opcode == WEBSOCKET_OPCODE_TEXT) {
@@ -47,14 +59,14 @@ class ngSwoole
       }
     });
 
-    $this->http->on('open', function ($ws, $request) {
-      echo "Client: Connect\n";
-    });
+    // $this->http->on('open', function ($ws, $request) {
+    //   echo "Client: Connect\n";
+    // });
 
-    $this->http->on('close', function ($ws, $fd) {
-      unset($clients[$fd]);
-      echo "Client: Close\n";
-    });
+    // $this->http->on('close', function ($ws, $fd) {
+    //   unset($clients[$fd]);
+    //   echo "Client: Close\n";
+    // });
 
     $this->http->start();
   }
