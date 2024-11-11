88a3ca8f330eb1b296452948d784026090b834bb
杨志伟1 <752942639@qq.com>
2024-11-11 15:46:25 +0800

    dfsfs

---
+++ source/core/tool/Swoole.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace ng169\tool;
+
+use Swoole\Http\Server;
+use Swoole\WebSocket\Server;
+use Swoole\Server\Port;
+
+class Swoole
+{
+
+  public $http;
+  public function start($port)
+  {
+
+    $this->http = new \Server('0.0.0.0', $port);
+    $this->http->on('request', function ($request, $response) {
+      $response->end("Welcome to WebSocket chat room!");
+    });
+
+
+
+
+    $ws = $this->http->on('websocket', function ($ws, $frame) {
+      static $clients = [];
+      if ($frame->opcode == WEBSOCKET_OPCODE_TEXT) {
+        if ($frame->data == 'ping') {
+          $ws->push($frame->fd, 'pong');
+          return;
+        }
+        if ($frame->fd == -1) {
+          foreach ($clients as $fd) {
+            $ws->push($fd, $frame->data);
+          }
+        } else {
+          $clients[$frame->fd] = $frame->fd;
+          $ws->push($frame->fd, 'Welcome to WebSocket chat room!');
+          foreach ($clients as $fd) {
+            if ($fd != $frame->fd) {
+              $ws->push($fd, $frame->data);
+            }
+          }
+        }
+      }
+    });
+
+    $this->http->on('open', function ($ws, $request) {
+      echo "Client: Connect\n";
+    });
+
+    $this->http->on('close', function ($ws, $fd) {
+      unset($clients[$fd]);
+      echo "Client: Close\n";
+    });
+
+    $this->http->start();
+  }
+}
