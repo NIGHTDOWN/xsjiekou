acf7aafda50f5ae522471b09fc89adfb75507d8d
杨志伟1 <752942639@qq.com>
2024-11-16 01:09:06 +0800

    dasdasdad

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
 namespace ng169;
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    header('Access-Control-Allow-Methods: POST');
+    header('Access-Control-Allow-Headers: Content-Type');
+    header('Access-Control-Allow-Credentials: true');
+    header('Access-Control-Max-Age: 3600');
+    exit(0);
+}
 header('Access-Control-Allow-Origin:*'); 
 // 响应类型 
 header('Access-Control-Allow-Methods:PUT,GET,POST,OPTIONS'); 
