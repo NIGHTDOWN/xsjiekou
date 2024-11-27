a98ad751351c882b6eb9be61d206a4178d368aa6
杨志伟1 <752942639@qq.com>
2024-11-27 19:07:58 +0800

    dfgdgdg

--- source/control/apiv1base.php
+++ source/control/apiv1base.php
@@ -248,8 +248,8 @@ class apiv1base extends general
             $hash_data .= htmlspecialchars_decode($v);
         }
         //  d($AllPar);
-        // d($hash_data);
-        //  d($secret);
+        d($hash_data);
+         d($secret);
         $_apiSign = hash_hmac('md5', $hash_data, $secret);
        
         if ($_apiSign != $apiSign) {
