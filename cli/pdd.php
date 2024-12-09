1f345f39617e649cd147d63086c790a9cc6a9eda
杨志伟1 <752942639@qq.com>
2024-12-09 14:31:42 +0800

    sdfsf

--- cli/pdd.php
+++ cli/pdd.php
@@ -43,9 +43,9 @@ class pdd extends \ng169\cli\Clibase
 
     private function _start()
     {
-      
+
         $this->init();
-        $tk='x_eyJ0IjoiRFZFcGo4TXhMRzB6V3p4aGlrcjZVWG0vZDN0N1F5bmhaSjhpaWdUdklYdjNyaW5mWUdXclJ0dzBrQTg3aWMrTCIsInYiOjEsInMiOjEwMDYsInUiOjM1MDg2MDIyMzUwfQ==';
+        $tk = 'x_eyJ0IjoiRFZFcGo4TXhMRzB6V3p4aGlrcjZVWG0vZDN0N1F5bmhaSjhpaWdUdklYdjNyaW5mWUdXclJ0dzBrQTg3aWMrTCIsInYiOjEsInMiOjEwMDYsInUiOjM1MDg2MDIyMzUwfQ==';
         $pddhead = array(
             "p-appname" => "DDStore",
             "cookie" => "SUB_PASS_ID=$tk",
@@ -96,17 +96,18 @@ class pdd extends \ng169\cli\Clibase
             "device" => "LM-G820"
         );
         $this->getenc();
-    //     $this->spiner->setproxy("192.168.10.11","6666");
-    //   $url="https://mdkd-api.pinduoduo.com/api/orion/op/cabinet/in/new";
-    //     $ret = $this->post($url, '{"is_virtual":"false","customer_type":"0","waybill_code":"JT5500393983848","mobile_type":"0","temporary_mobile_status":"false","modify_wp":"false","modify_waybill_code":"false","type":"1","modify_customer_name":"false","receiver_type":"0","pickup_code":"6-8-1","courier_id":"0","mobile_last_four":"","name_source":"100","wp_code":"JTSD","wp_name":"极兔速递","mobile":"13112234215","is_manual_input":"false","shelf_number":"6-8","modify_pickup_code":"false","in_cabinet_type":"1","modify_mobile":"true","receiver_type_confirm":"false","confirm_flag":"false","customer_name":"yang","extend_type":"1","device":"LM-G820"}');
-    //     d($ret);
+        //     $this->spiner->setproxy("192.168.10.11","6666");
+        //   $url="https://mdkd-api.pinduoduo.com/api/orion/op/cabinet/in/new";
+        //     $ret = $this->post($url, '{"is_virtual":"false","customer_type":"0","waybill_code":"JT5500393983848","mobile_type":"0","temporary_mobile_status":"false","modify_wp":"false","modify_waybill_code":"false","type":"1","modify_customer_name":"false","receiver_type":"0","pickup_code":"6-8-1","courier_id":"0","mobile_last_four":"","name_source":"100","wp_code":"JTSD","wp_name":"极兔速递","mobile":"13112234215","is_manual_input":"false","shelf_number":"6-8","modify_pickup_code":"false","in_cabinet_type":"1","modify_mobile":"true","receiver_type_confirm":"false","confirm_flag":"false","customer_name":"yang","extend_type":"1","device":"LM-G820"}');
+        //     d($ret);
     }
-    public function getenc(){
-        $this->spiner->setproxy("192.168.10.11","6666");
-        $url="http://121.199.168.122:8090/edge/call";
-        $pddhead = array( "Content-Type" => "application/json;charset=utf-8",);
+    public function getenc()
+    {
+        $this->spiner->setproxy("192.168.10.11", "6666");
+        $url = "http://121.199.168.122:8090/edge/call";
+        $pddhead = array("Content-Type" => "application/json;charset=utf-8",);
         $this->head($pddhead);
-        $data='{
+        $data = '{
     "group": "com.xunmeng.station",
     "action": "api_ddmc_anti_gen",
     "clientid": "aUzXyNcq",
